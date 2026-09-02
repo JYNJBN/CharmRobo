73c7edf04a2ada9f765ed5b84fcb96031c82ffac
weixin_47995221 <weixin_47995221@noreply.gitcode.com>
2026-09-02 11:16:52 +0800

    fix configpy添加Milvus服务配置

--- app/core/config.py
+++ app/core/config.py
@@ -2,6 +2,7 @@ from pydantic import SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+
 class Settings(BaseSettings):
     app_name: str="Charming Device API"
     debug: bool=True
